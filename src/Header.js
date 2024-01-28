cf7e4d53e2fbe2544608aa74abe57535fe8faeed
yanzhanglovx <yanzhanglovx@gmail.com>
2024-01-28 11:10:04 +0800

    Initialize

---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div>
+            <div className="header__left">
+                <img src="" alt="" />
+
+                <div className="header__search">
+                    {/* SearchIcon */}
+                    <input type="text" />
+                </div>
+
+            </div>
+
+            <div className="header__right">
+
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
