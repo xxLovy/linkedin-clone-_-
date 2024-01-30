78273925eea5de15d621fe40bcf77df91764d3f8
xxLovy <1xxlovyxx1@gmail.com>
2024-01-30 11:17:46 +0800

    modules

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from "@material-ui/icons/Search"
+import HeaderOption from './HeaderOption'
+import HomeIcon from "@material-ui/icons/Home"
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount"
 
 function Header() {
     return (
-        <div>
+        <div className='header'>
             <div className="header__left">
-                <img src="" alt="" />
+                <img src="./linkedin_3536505.png" alt="linkedin picture" />
 
                 <div className="header__search">
-                    {/* SearchIcon */}
+                    <SearchIcon />
                     <input type="text" />
                 </div>
 
             </div>
 
             <div className="header__right">
-
+                <HeaderOption Icon={HomeIcon} title="Home" />
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+                <HeaderOption />
 
             </div>
 
